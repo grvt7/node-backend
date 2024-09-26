ea38a007773d04797378ff5312083666ae7b3955
grvt <r.garvit7@gmail.com>
2024-09-27 02:21:21 +0530

    user video model-jwt-bcrypt

--- src/app.js
+++ src/app.js
@@ -8,19 +8,19 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  })
+  }),
 );
 
 app.use(
   express.json({
     limit: '16kb',
-  })
+  }),
 );
 
 app.use(
   express.urlencoded({
     extended: true,
-  })
+  }),
 );
 
 app.use(express.static('public'));
