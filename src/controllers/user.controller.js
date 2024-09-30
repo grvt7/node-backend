e158fbdd94fbbb413a84fce61a5976411567c217
grvt <r.garvit7@gmail.com>
2024-10-01 00:18:47 +0530

    aggregators mongodb subscribers/subbedTo-needsRivision

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -291,6 +291,65 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, 'Cover Image Updated Successfully'));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) throw new ApiError(400, 'Username is Missing');
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username.toLowerCase(),
+      },
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'channel',
+        as: 'subscribers',
+      },
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'subscriber',
+        as: 'subscribedTo',
+      },
+      $addFields: {
+        subscribersCount: {
+          $size: '$subscribers',
+        },
+        subscribedToCount: {
+          $size: '$subscribedTo',
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, '$subscribers.subscriber'],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribedToCount: 1,
+        subscribersCount: 1,
+        isSubscribed: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) throw new ApiError(404, 'Channel Does Not Exist');
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], 'User Channel Fetched Successfully'),
+    );
+});
+
 export {
   registerUser,
   loginUser,
@@ -301,4 +360,5 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChannelProfile,
 };
